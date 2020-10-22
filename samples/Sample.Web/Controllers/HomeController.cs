76d3af56f8542bb43cec23eefc6184057e9dab18
Edward Henn <emhenn@ucdavis.edu>
2020-10-22 14:01:20 -0700

    add sample project to demonstrate basic usage of mediasite client

---
+++ samples/Sample.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MediasiteUtil;
+using MediasiteUtil.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Sample.Web.Models;
+
+namespace Sample.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        private readonly MediasiteClient _mediasite;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config, MediasiteClient mediasite)
+        {
+            _logger = logger;
+            _config = config;
+            _mediasite = mediasite;
+            mediasite.Config = config.GetSection("MediasiteConfig").Get<MediasiteConfig>();
+        }
+
+        public IActionResult Index()
+        {
+            var folder = _mediasite.FindFolder("Mediasite");
+            return View(folder);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
